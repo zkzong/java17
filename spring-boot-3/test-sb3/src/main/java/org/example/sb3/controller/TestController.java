ce70a288c19948909a9104527d5160a0fb85ea7d
zkzong <zongzhankui@hotmail.com>
2023-12-26 14:29:42 +0800

    spring boot 3

--- spring-boot-3/test-sb3/src/main/java/org/example/sb3/controller/TestController.java
+++ spring-boot-3/test-sb3/src/main/java/org/example/sb3/controller/TestController.java
@@ -1,18 +1,23 @@
 package org.example.sb3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author zong
+ */
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-    @RequestMapping("/get")
+    @GetMapping("/get")
     public String get() {
         return "success";
     }
 
-    @RequestMapping("/post")
+    @PostMapping("/post")
     public String post() {
         return "success";
     }
