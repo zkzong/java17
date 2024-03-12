a8db440d2df514dd4d2f47d5c1d1ff70d515cc83
zkzong <zongzhankui@hotmail.com>
2024-03-12 21:03:20 +0800

    test-sb3

--- spring-boot-3/test-sb3/src/main/java/org/example/sb3/controller/TestController.java
+++ spring-boot-3/test-sb3/src/main/java/org/example/sb3/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.example.sb3.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,17 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author zong
  */
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/get")
     public String get() {
+        log.info("get");
         return "success";
     }
 
     @PostMapping("/post")
     public String post() {
+        log.info("post");
         return "success";
     }
 
