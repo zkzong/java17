18c6decc2fcb29a51fef25dccd416a8d0819349e
zkzong <zongzhankui@hotmail.com>
2024-03-14 19:13:34 +0800

    mybatis

--- spring-boot-3/db/mysql/mybatis/mybatis-sb3/src/main/java/org/example/sb3/mybatis/domain/User.java
+++ spring-boot-3/db/mysql/mybatis/mybatis-sb3/src/main/java/org/example/sb3/mybatis/domain/User.java
@@ -3,6 +3,8 @@ package org.example.sb3.mybatis.domain;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.Date;
+
 @Data
 @ToString
 public class User {
@@ -10,5 +12,7 @@ public class User {
     private Long id;
     private String userName;
     private Integer age;
+    private Date createTime;
+    private Date updateTime;
 
 }
--- spring-boot-3/db/mysql/mybatis/mybatis-sb3/src/main/resources/db/schema.sql
+++ spring-boot-3/db/mysql/mybatis/mybatis-sb3/src/main/resources/db/schema.sql
@@ -3,8 +3,10 @@
 -- use mybatis;
 
 drop table if exists t_user;
-CREATE TABLE t_user(
-  id int not null auto_increment primary key comment 'id',
-  user_name varchar(20) not null comment '姓名',
-  age int not null comment '年龄'
+CREATE TABLE t_user (
+    id          int         not null auto_increment primary key comment 'id',
+    user_name   varchar(20) not null comment '姓名',
+    age         int         not null comment '年龄',
+    create_time timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+    update_time timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间'
 ) comment '用户表';
