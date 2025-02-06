d46cd4594833615b0e77219a2a1e6f85a2fd6ccb
zkzong <zongzhankui@hotmail.com>
2025-02-06 23:09:36 +0800

    kafka

--- spring-boot-3/mq/kafka/kafka-spring-boot/src/main/java/com/example/kafka/controller/auto/AutoConsumer.java
+++ spring-boot-3/mq/kafka/kafka-spring-boot/src/main/java/com/example/kafka/controller/auto/AutoConsumer.java
@@ -3,6 +3,7 @@ package com.example.kafka.controller.auto;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +14,7 @@ public class AutoConsumer {
      * 使用 message 接受消息
      */
     @KafkaListener(topics = "auto-topic", groupId = "group_id_1")
-    public void consume1(String message) {
+    public void consume(String message) {
         log.info(String.format("group_id_1 -> $$ -> Consumed Message -> %s", message));
     }
 
@@ -21,8 +22,16 @@ public class AutoConsumer {
      * 使用 record 接受消息
      */
     @KafkaListener(topics = "auto-topic", groupId = "group_id_2")
-    public void consume2(ConsumerRecord<String, String> record) {
+    public void consume(ConsumerRecord<String, String> record) {
         log.info(String.format("group_id_2 -> $$ -> Consumed Message -> %s", record));
+    }
+
+    /**
+     * 使用 Payload 接受消息
+     */
+    @KafkaListener(topics = "auto-topic", groupId = "group_id_3")
+    public void consumePayload(@Payload String payload) {
+        log.info(String.format("group_id_3 -> $$ -> Consumed Message -> %s", payload));
     }
 
 }
