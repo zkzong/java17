430f21cef139d4005181648a838eed3c1ea14b20
zkzong <zongzhankui@hotmail.com>
2025-02-28 23:29:30 +0800

    flowable

---
+++ spring-boot-3/bpm/flowable/src/test/java/org/example/flowable/FlowableTest.java
@@ -0,0 +1,21 @@
+package org.example.flowable;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: zongz
+ * @Date: 2025-02-22
+ */
+public class FlowableTest {
+
+    /***
+     *
+     * 部署流程到数据库中
+     * 在非Spring环境下的应用
+     */
+    @Test
+    public void deployFlow() {
+
+    }
+
+}
