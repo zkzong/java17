ab29c5317aa3ded5e8974242c7a50598b31addb9
zkzong <zongzhankui@hotmail.com>
2025-03-26 23:48:25 +0800

    shardingsphere

---
+++ spring-boot-3/db/mysql/shardingsphere/src/main/java/org/example/sb3/ss/ShardingSphereApplication.java
@@ -0,0 +1,15 @@
+package org.example.sb3.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zongz
+ * @Date: 2025-03-26
+ */
+@SpringBootApplication
+public class ShardingSphereApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingSphereApplication.class, args);
+    }
+}
