c282cb2bfcb15c44fe23d93dbe5e77fa0b0ee8bf
zkzong <zongzhankui@hotmail.com>
2025-05-14 23:20:26 +0800

    webflux

---
+++ spring-boot-3/test-sb3/src/main/java/org/example/sb3/webflux/HelloHandle.java
@@ -0,0 +1,18 @@
+package org.example.sb3.webflux;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class HelloHandle {
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject("Hello, This is a Spring Boot WebFlux Project!"));
+    }
+
+}
---
+++ spring-boot-3/test-sb3/src/main/java/org/example/sb3/webflux/HelloRouter.java
@@ -0,0 +1,21 @@
+package org.example.sb3.webflux;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class HelloRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> routeHello(HelloHandle helloHandle) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/hello")
+                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), helloHandle::hello);
+    }
+
+}
