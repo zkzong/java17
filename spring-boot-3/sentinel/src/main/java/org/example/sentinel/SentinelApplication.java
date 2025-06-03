54aab105f352daa940408bf96a4032ff46de8928
zkzong <zongzhankui@hotmail.com>
2025-06-03 23:05:09 +0800

    sentinel

---
+++ spring-boot-3/sentinel/src/main/java/org/example/sentinel/SentinelApplication.java
@@ -0,0 +1,11 @@
+package org.example.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+}
