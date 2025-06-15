d73f8dd3f70dc9f3e9803cb48a79c880007f83d4
zkzong <zongzhankui@hotmail.com>
2025-06-15 08:36:53 +0800

    springdoc

--- spring-boot-3/test-sb3/src/main/java/org/example/sb3/pojo/springdoc/User.java
+++ spring-boot-3/test-sb3/src/main/java/org/example/sb3/pojo/springdoc/User.java
@@ -9,10 +9,10 @@ import java.io.Serializable;
 @Schema(description = "用户")
 public class User implements Serializable {
 
-    @Schema(name = "id", description = "id", required = true)
+    @Schema(name = "id", title = "id", description = "id", format = "int32", required = true, requiredMode = Schema.RequiredMode.REQUIRED)
     private Long id;
 
-    @Schema(name = "用户姓名", description = "用户姓名", required = true)
+    @Schema(name = "用户姓名", title = "用户姓名", description = "用户姓名", example = "张/刘", accessMode = Schema.AccessMode.READ_ONLY, required = true, requiredMode = Schema.RequiredMode.REQUIRED)
     private String username;
 
     @Schema(name = "密码", description = "密码")
@@ -27,6 +27,10 @@ public class User implements Serializable {
     @Schema(name = "手机号", description = "手机号")
     private String phone;
 
-    public User(Long id, String userName, String password) {}
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
 
 }
