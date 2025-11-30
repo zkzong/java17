5deff0d59f860963e24feb97f9fb57a31419710a
zkzong <zongzhankui@hotmail.com>
2025-11-30 18:06:00 +0800

    canal

---
+++ spring-boot-3/db/mysql/canal/src/test/java/org/example/canal/CanalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
